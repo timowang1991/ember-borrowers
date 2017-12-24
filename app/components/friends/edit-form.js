151277bbe7848a42f436feeb166c9ebf9871bfe9
Timo Wang <chiuanw@yahoo-inc.com>
2017-12-24 17:55:09 +0800

    add a friend form

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -1,4 +1,51 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
+    isValid: Ember.computed(
+        'model.email',
+        'model.firstName',
+        'model.lastName',
+        'model.twitter',
+        {
+            get() {
+                return !Ember.isEmpty(this.get('model.email')) &&
+                    !Ember.isEmpty(this.get('model.firstName')) &&
+                    !Ember.isEmpty(this.get('model.lastName')) &&
+                    !Ember.isEmpty(this.get('model.twitter'));
+            }
+        }
+    ),
+    actions: {
+        save() {
+            console.log('+- save action in edit-form component');
+            if (this.get('isValid')) {
+                this.get('model').save().then((friend) => {
+                    //
+                    // This function gets called if the HTTP request succeeds
+                    //
+                    //
+                    // We are calling the save action passed down when rendering
+                    // the component: action=(action "save")
+                    return this.save(friend);
+                }, (err) => {
+                    //
+                    // This gets called if the HTTP request fails.
+                    //
+                    this.set('errorMessage', 'there was something wrong saving the model: ' + err);
+                });
+            } else {
+                this.set('errorMessage', 'You have to fill all the fields');
+            }
+        },
+        cancel() {
+            console.log('+- cancel action in edit-form component');
+
+            //
+            // We are calling the cancel action passed down when rendering the
+            // component: action=(action "cancel")
+            //
+            this.cancel();
+        }
+    }
 });
---
+++ app/controllers/friends/new.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    actions: {
+        save(model) {
+            console.log('+- save action in friends new controller');
+
+            this.transitionToRoute('friends.show', model);
+        },
+        cancel() {
+            console.log('+- cancel action in friends new controller');
+        }
+    }
+});
