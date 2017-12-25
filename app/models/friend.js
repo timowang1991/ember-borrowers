9cc50b34db7eebcf0daf817b66d02b51519647f9
Timo Wang <chiuanw@yahoo-inc.com>
2017-12-25 21:24:52 +0800

    add Ember computed friend fullName

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,8 +1,14 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
-  twitter: DS.attr('string')
+  twitter: DS.attr('string'),
+  fullName: Ember.computed('firstName', 'lastName', {
+    get() {
+      return this.get('firstName') + ' ' + this.get('lastName');
+    }
+  })
 });
