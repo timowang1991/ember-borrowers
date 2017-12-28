86267ef102a75c9a3b759f819176e1800273de32
Timo Wang <chiuanw@yahoo-inc.com>
2017-12-28 14:54:14 +0800

    css and fonts imported

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,24 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.eot', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.svg', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff2', {
+    destDir: 'assets/font'
+  });
+  
+  app.import('vendor/basscss.min.css');
 
   return app.toTree();
 };
