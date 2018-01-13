55ba2b51bcff213859a1853f65413a9010560eb1
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-13 17:27:03 +0800

    able to edit loans note

--- app/components/loans/edit-form.js
+++ app/components/loans/edit-form.js
@@ -1,4 +1,39 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
+    //
+    // By default the store is not injected into components, so we use
+    // the "inject.service" helper to make it available
+    //
+    store: Ember.inject.service(),
+    articles: Ember.computed({
+        get() {
+            //
+            // Since we are using Ember.inject.service, we need to call the
+            // store using the get helper
+            //
+            return this.get('store').findAll('article');
+        }
+    }).readOnly(),
+
+    //
+    // Save and cancel are not declared inside actions key, we'll be
+    // using it as closure actions
+    //
+    save() {
+        //
+        // We probably want to verify here that the model has an article
+        // before saving
+        //
+        this.get('model').save().then((model) => {
+            this.back(model.get('friend'));
+        }, () => {
+            this.set('errorMessage', 'there was something wrong saving the loan');
+        });
+    },
+
+    cancel() {
+        this.back(this.get('model.friend'));
+    }
 });
