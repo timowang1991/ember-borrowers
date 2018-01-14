0174031b6c841da5db6819fe78af6c249264e85d
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-14 20:16:18 +0800

    does not change anything if cancel

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    resetController(controller, isExiting) {
+        if (isExiting) {
+            var model = controller.get('model');
+            model.rollbackAttributes(); // This will return the attributes to their initial state if the model hasDirtyAttributes
+        }
+    }
 });
