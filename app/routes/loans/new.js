ef0b0bcc493fa9be7436eb03bce3cb43b8a1bfa4
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-14 20:26:22 +0800

    add resetController to loans/new

--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -5,5 +5,15 @@ export default Route.extend({
         return this.store.createRecord('loan', {
             friend: this.modelFor('friends/show')
         });
+    },
+
+    resetController: function (controller, isExiting) {
+        if (isExiting) {
+            var model = controller.get('model');
+
+            if (model.get('isNew')) {
+                model.destroyRecord();
+            }
+        }
     }
 });
