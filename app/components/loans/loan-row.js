f21e83b91e007d77712b8c0f3ef49876a807469b
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-16 22:15:48 +0800

    add observer to loan.returned

--- app/components/loans/loan-row.js
+++ app/components/loans/loan-row.js
@@ -1,6 +1,11 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
     tagName: 'tr',
-    loan: null  // passed-in
+    loan: null,  // passed-in
+    stateChanged: Ember.observer('loan.returned', function () {
+        var loan = this.get('loan');
+        console.log('OMG Expensive operation because loan state changed');
+    }),
 });
