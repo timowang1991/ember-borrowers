f7c1fd5884931edbfaf816168fa09dd7c5faa54f
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-16 21:38:30 +0800

    side loading relationships

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -2,6 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.store.findAll('friend');
+        // return this.store.findAll('friend');
+
+        //
+        // We now use store.query and pass include in the options
+        //
+
+        return this.store.query('friend', { include: 'loans,loans.article' });
     }
 });
