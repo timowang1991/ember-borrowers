2daa4e26e3443476ad26e406e023e93da7fea219
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-16 22:17:40 +0800

    addObserver through Ember object life cycle

--- app/components/loans/loan-row.js
+++ app/components/loans/loan-row.js
@@ -1,11 +1,20 @@
-import Ember from 'ember';
+// import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
     tagName: 'tr',
     loan: null,  // passed-in
-    stateChanged: Ember.observer('loan.returned', function () {
-        var loan = this.get('loan');
+
+    // stateChanged: Ember.observer('loan.returned', function () {
+    //     var loan = this.get('loan');
+    //     console.log('OMG Expensive operation because loan state changed');
+    // }),
+
+    init() {
+        this._super(...arguments);
+        this.addObserver('loan.returned', this, this.stateChanged);
+    },
+    stateChanged() {
         console.log('OMG Expensive operation because loan state changed');
-    }),
+    }
 });
